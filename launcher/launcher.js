8beed2dac236203bb7789f9082cc13b8d0cfd251
Matthias Hannig <matthias@hannig.cc>
2015-04-15 10:59:31 +0200

    Renamed to launchify

---
+++ launcher/launcher.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * Launchfit Application launcher and Updater
+ * ------------------------------------------
+ *
+ * (c) 2015 Matthias Hannig
+ */
+
+var fs      = require('fs');
+var path    = require('path');
+
+
+// Check if all directories are present
+
+// Check filesystem and test if there is a 
+// launchfit.yml in current/ present.
+
+// Parse it.
+
+// Nothing there? Check argv.
+
+// is URL? 
+
+// Great! Download it!
+
+
+
+
+
