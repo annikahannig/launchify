ef0c6b08be68ed81fccc95e8d046f1745242d158
Matthias Hannig <matthias@hannig.cc>
2015-04-16 15:00:12 +0200

    download with progress.

--- launcher/downloader/download-release.js
+++ launcher/downloader/download-release.js
@@ -10,51 +10,92 @@
 // == Load libraries
 var logSymbols = require('log-symbols');
 var Promise    = require('promise');
-var http       = require('http');
 var fs         = require('fs');
 
+var ProgressBar  = require('progress');
+var request      = require('request');
+
+// == Helper
+
 /**
- * Download release archive and signature.
+ * Download files
+ * @return Promise
+ */
+var downloadFile = function( filename, url ) {
+  var promise = new Promise(function(resolve, reject){
+
+    var file = fs.createWriteStream(
+      process.cwd() + '/tmp/downloads/' + filename
+    );
+
+    // Start download with progress bar
+    request
+      .get(url)
+      .on('response',function(res) {
+        // Create progress bar 
+        var total = parseInt(res.headers['content-length'], 10); 
+        var bar = new ProgressBar('  [:bar] :percent :etas', {
+          complete: '=',
+          incomplete: ' ',
+          width: 40,
+          total: total
+        });
+
+        res.on('data', function(chunk) {
+          bar.tick(chunk.length);
+        });
+      })
+      .on('error',function(err) {
+        reject(err);
+      })
+      .pipe(file)
+      .on('error', function(err) {
+        reject(err);
+      })
+      .on('close', function() {
+        console.log(logSymbols.success + ' finished downloading ' + filename);
+        resolve(filename);
+      });
+  });
+  return promise;
+};
+
+
+/**
+ * Download archive signature.
+ */
+
+
+
+/**
+ * Download release archive.
  *
  * @return Promise
  */
 var loadRelease = function(repositoryUrl) {
   return function(release) {
-    console.log(logSymbols.info + ' Downloading release...');
+    console.log(logSymbols.info + ' Downloading reelease archive');
     var promise = new Promise(function(resolve, reject){
-    
-      var archiveUrl    = repositoryUrl + '/' + release.file;
-      var signatureUrl  = repositoryUrl + '/' + release.file + '.sig';
-
-      var archiveFile   = fs.createWriteStream(
-        process.cwd() + '/tmp/downloads/' + release.file
-      );
-
-      var signatureFile = fs.createWriteStream(
-        process.cwd() + '/tmp/downloads/' + release.file + '.sig'
-      );
       
-      var reqArchive = http.get(archiveUrl, function(res){
-        if(res.statusCode == 200) {
-          res.pipe(archiveFile);
-        }
-        else {
-          console.error(logSymbols.error + ' Could not download archive.');
-          reject('archive download filed');
-        }
+      // Download release
+      var archiveUrl       = repositoryUrl + '/' + release.file;
+      var archiveFilename  = release.file;
+  
+      var sigUrl       = repositoryUrl + '/' + release.file + '.sig';
+      var sigFilename  = release.file + '.sig';
 
-        res.on('end', function(){
-          console.log(logSymbols.success + ' Archive ' + release.file + ' downloaded.');
-        });
-      });
-
-      var reqSignature = http.get(signatureUrl, function(res){
-        res.pipe(signatureFile);
-        res.on('end', function(){
-          console.log(logSymbols.success + ' Archive signature downloaded.');
-        });
-      });
-
+      Promise.all([
+        downloadFile(archiveFilename, archiveUrl        ),
+        downloadFile(sigFilename,     sigUrl,      false) // no progress
+      ])
+        .then(
+          function(result){
+            resolve(release);
+          },
+          function(err){
+            reject(err);
+          }
+        );
     });
     return promise;
   };
