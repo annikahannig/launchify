183bc52150176fb48ea127584a8f37f17f221cc6
Matthias Hannig <matthias@hannig.cc>
2015-04-17 23:15:43 +0200

    added update task

---
+++ launcher/cli/update.js
@@ -0,0 +1,61 @@
+'use strict';
+
+/**
+ * Update the app
+ *
+ * Afert downloading the RELEASE metadata it 
+ * checks if there is an update ready.
+ * 
+ * (c) 2015 Matthias Hannig
+ */
+
+// == Libraries
+var sym = require('log-symbols');
+
+// == Modules
+var currentRelease = require('../release/current-release');
+
+var parseRelease    = require('../release/parse-release');
+var needsUpdate     = require('../release/needs-update');
+
+var downloadMeta    = require('../downloader/download-release-meta');
+var downloadRelease = require('../downloader/download-release');
+var unpack          = require('../archiver/unpack');
+var verify          = require('../verifier/verify');
+var install         = require('../installer/install');
+
+// == Update application (if needed)
+var cli_update = function(argv) {
+  var _release = currentRelease();
+  var repositoryUrl = _release.updates.repository;
+  
+  // Start (and verify) download
+  downloadMeta(repositoryUrl)
+    .then(needsUpdate(_release.version))
+    .then(downloadRelease(repositoryUrl))
+    .then(unpack())
+    .then(verify())
+    .then(install())
+    .then(function(release){
+      console.log(sym.success + 
+        ' ' + _release.app.name + ' successfully updated: ' +
+        _release.version + ' -> ' + release.version
+      );
+    }, 
+    function(err) {
+      if(err == 'release_is_up_to_date') {
+        console.log(sym.success + 
+          ' ' + _release.app.name + '(' + _release.version + ') ' +
+          'is up to date.'
+        );
+      }
+      else {
+        console.log(sym.error + ' ' + err);
+      }
+    });
+
+};
+
+// == Export
+module.exports = cli_update;
+
