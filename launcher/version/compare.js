56a082c1f01f10b67081f081b9a2323e21631dba
Matthias Hannig <matthias@hannig.cc>
2015-04-17 16:59:50 +0200

    added missing semicolons

--- launcher/version/compare.js
+++ launcher/version/compare.js
@@ -38,22 +38,22 @@ var compare = function(a, b) {
   if( v1[0] == v2[0] && v1[1] == v2[1] && v1[2] < v2[2] ) {  return -1; }
 
   throw "This should never happen.";
-}
+};
 
 // == Newer 
 var isNewer = function(a,b) {
   return compare(a,b) == 1;
-}
+};
 
 // == Older
 var isOlder = function(a,b) {
   return compare(a,b) == -1;
-}
+};
 
 // == Equal
 var isEqual = function(a,b) {
-  return compare(a,b) == 0;
-}
+  return compare(a,b) === 0;
+};
 
 // == Export
 module.exports = compare;
